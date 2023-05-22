2dd02488646beab06a1e33a65906411f984db5bd
Klymenko Illia <sovuh2703@gmail.com>
2023-05-22 15:01:22 +0300

    Created increment_views function

--- shoes/views.py
+++ shoes/views.py
@@ -1,4 +1,6 @@
 from django.db.models import Subquery, OuterRef
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from shoes.models import Shoe, Brand
@@ -11,7 +13,16 @@ class ShoeViewSet(ModelViewSet):
     ).prefetch_related('qty')
     serializer_class = ShoeSerializer
 
+    @action(detail=True, methods=['post'])
+    def increment_views(self, request, pk=None):
+        shoe = self.get_object()
+        shoe.views += 1
+        shoe.save()
+        return Response({'message': 'Views count incremented'})
+
 
 class BrandViewSet(ModelViewSet):
     queryset = Brand.objects.all().values('name')
     serializer_class = BrandSerializer
+
+
