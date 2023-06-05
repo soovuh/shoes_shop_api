588ce073b17b65967e408266f82065678581babb
Klymenko Illia <sovuh2703@gmail.com>
2023-06-05 18:57:25 +0300

    Created new user fields

--- accounts/models.py
+++ accounts/models.py
@@ -8,6 +8,8 @@ from .managers import CustomUserManager
 
 class CustomUser(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+    address = models.CharField(max_length=255, blank=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(_('is active'), default=True)
     USERNAME_FIELD = 'email'
